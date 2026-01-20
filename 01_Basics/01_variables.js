735d647381c47de6a7212a6e575a766ad5ea6e90
Fahad Shah <fhd10839@gmail.com>
2026-01-20 06:47:14 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountid=2345
+let accountemail="fhd108939@gmail.com"
+var accountpass="4567"
+accountCity="Kohat"
+ let accountStatement;
+
+// accountid=34565
+//console.log(accountpass);
+console.table([accountid,accountemail,accountpass,accountCity,accountStatement])
