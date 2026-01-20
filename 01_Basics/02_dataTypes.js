71acb0dc0c281186cf57ad239b1501c4b193db21
Fahad Shah <fhd10839@gmail.com>
2026-01-20 06:39:20 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"
+
+console.log(3+3);
+
+let name="Fahad"
+let age=22
+let isLogged=true
+
+//String(use in double qoutes--ziada preferable hai)
+//boolean==>true/false 
+//Type of null is object
+
+console.log(typeof age)
+
+
