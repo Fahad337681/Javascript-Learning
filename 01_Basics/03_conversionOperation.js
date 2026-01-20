b04464d08eec04c50febeef17414caacfff6ec04
Fahad Shah <fhd10839@gmail.com>
2026-01-20 10:10:44 +0000

    conversionOperations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score=56
+
+console.log(typeof score);
+
+let valueInNumber=String(score)
+console.log(typeof valueInNumber);
+
+// "33" ==>33
+ 
+
+let isLoggedIn=1
+
+let booleanIs=Boolean(isLoggedIn)
+console.log(booleanIs);
+
+let Number=33
+let newString=String(Number)
+console.log(newString)
+console.log(typeof newString);
+
+
